22042783c3e1b0b7d89a2abc256eefead917ff76
pandaspartan2 <90705933+pandaspartan2@users.noreply.github.com>
2021-11-10 09:09:35 -0600

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by AMStudent on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
