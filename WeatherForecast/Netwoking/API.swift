ff58fe2cb73506699a05d940c2b09932acea497c
pandaspartan2 <90705933+pandaspartan2@users.noreply.github.com>
2021-11-10 10:04:19 -0600

    added api key

---
+++ WeatherForecast/Netwoking/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  WeatherForecast
+//
+//  Created by AMStudent on 11/10/21.
+//
+
+import Foundation
+
+struct API {
+    static let key = "151475b0ea9b7b2592ab45f8de7357ad"
+}
